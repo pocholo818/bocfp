28e2e768691c9b540a22f2e0e526334692ef7b05
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2022-10-11 19:56:37 +0800

    starter files

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BOCFP</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+  </head>
+  
+  <body class="text-center">
+    
+    <main class="form-signin">
+        <form>
+            <img class="mb-4" src="photo/old cab logo.png" alt="" width="72" height="57">
+            <h1 class="h3 mb-3 fw-normal">Welcome to BOCFP</h1>
+
+            <div class="form-floating">
+                <input type="email" class="form-control" placeholder="Email">
+                <label>Email address</label>
+            </div>
+
+            <div class="form-floating">
+                <input type="password" class="form-control" placeholder="Password">
+                <label>Password</label>
+            </div>
+
+            <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+            </div>
+
+            <button class="w-100 btn btn-lg btn-success" type="submit">Sign in</button>
+        </form>
+    </main>
+
+  </body>
+</html>
