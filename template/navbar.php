a3c580927f410425fbc280a673d0d2662c529fe2
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2022-10-15 23:30:17 +0800

    rough navbar made

---
+++ template/navbar.php
@@ -0,0 +1,16 @@
+<head>
+  <link rel="stylesheet" href="../css/sidebar.css">
+  <script src="../js/sidebar.js"></script>
+</head>
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Announcements</a>
+  <a href="#">Child Record</a>
+  <a href="#">Purok Health Workers</a>
+  <a href="#">Logout</a>
+</div>
+
+<div id="main">
+  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; BOCFP</span>
+</div>
